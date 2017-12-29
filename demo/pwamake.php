112b360f72ea901ff14e20961d8f5ff256b44b30
Alexander Selifonov <as-works@narod.ru>
2017-12-29 17:00:46 +0200

    Update pwamake.php

--- demo/pwamake.php
+++ demo/pwamake.php
@@ -6,5 +6,5 @@
 * @author Alexander Selifonov, <alex [at] selifan {dot} ru>
 * @link https://github.com/selifan/PWAssist
 */
-include_once('PWAssist.php');
+include_once('../src/PWAssist.php');
 PWAssist::init();
